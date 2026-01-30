72ba3abaa0b3bf9da4a88de0d3a507d477b472bf
ssamucr <ssamucr@gmail.com>
2026-01-29 22:09:45 -0500

    Add new models to API exports
    - Add CompromisoRecurrente, PlanQuincenal, MovimientoDeuda, and GastoPlanificado to models init
    - Update __all__ export list with new models

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -5,5 +5,10 @@ from api.models.transaccion import Transaccion
 from api.models.subcuenta import Subcuenta
 from api.models.movimiento_subcuenta import MovimientoSubcuenta
 from api.models.deuda import Deuda
+from api.models.compromiso_recurrente import CompromisoRecurrente
+from api.models.plan_quincenal import PlanQuincenal
+from api.models.movimiento_deuda import MovimientoDeuda
+from api.models.gasto_planificado import GastoPlanificado
+    
 
-__all__ = ["Usuario", "Cuenta", "Categoria", "Transaccion", "Subcuenta", "MovimientoSubcuenta", "Deuda"]
+__all__ = ["Usuario", "Cuenta", "Categoria", "Transaccion", "Subcuenta", "MovimientoSubcuenta", "Deuda", "CompromisoRecurrente", "PlanQuincenal", "MovimientoDeuda", "GastoPlanificado"]
